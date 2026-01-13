a940b7425a3a343a4b81062b64c9f768c7c1c6e8
Avilash Achar <avilashachar100@gmail.com>
2026-01-13 11:27:55 +0530

    Remove exposed SECRET_KEY

--- monthly_challenges/settings.py
+++ monthly_challenges/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x!eawjq1=uhrd$lhqn+6r=y=x1pwj*1lopi&^7bb7=9x1&22_j'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
